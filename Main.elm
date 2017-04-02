8c8f819187cfdb9ed8f72b7fada79cacb7ca399d
Tyler Haas <haasdevelopment@gmail.com>
2017-04-02 12:20:38 -0500

    add view and elm-mdl dependency

--- Main.elm
+++ Main.elm
@@ -5,6 +5,25 @@ import Html exposing (..)
 
 -- MODEL
 -- VIEW
+view =
+  Table.table []
+[ Table.thead []
+  [ Table.tr []
+    [ Table.th [] [ text "Material" ]
+    , Table.th [ ] [ text "Quantity" ]
+    , Table.th [ ] [ text "Unit Price" ]
+    ]
+  ]
+, Table.tbody []
+    (data |> List.map (\item ->
+       Table.tr []
+         [ Table.td [] [ text item.material ]
+         , Table.td [ Table.numeric ] [ text item.quantity ]
+         , Table.td [ Table.numeric ] [ text item.unitPrice ]
+         ]
+       )
+    )
+]
 -- UPDATE
 
 
