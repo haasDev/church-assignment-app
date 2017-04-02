fde3d2ac8870af48adc08a57b9f9fba3fc367a4c
Tyler Haas <haasdevelopment@gmail.com>
2017-04-02 12:07:03 -0500

    create initial beginner Program

---
+++ Main.elm
@@ -0,0 +1,17 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+-- MODEL
+-- VIEW
+-- UPDATE
+
+
+main : Html msg
+main =
+    beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
