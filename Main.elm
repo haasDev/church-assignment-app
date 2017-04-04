45b1af3ec6ee1efded375439ba8ba345a020f116
Tyler Haas <haasdevelopment@gmail.com>
2017-04-03 20:24:01 -0500

    add in support for datepicker

--- Main.elm
+++ Main.elm
@@ -1,39 +1,75 @@
 module Main exposing (..)
 
 import Date exposing (..)
-import Html exposing (beginnerProgram, div, Html, node, text)
+import DateTimePicker exposing (..)
+import Html exposing (program, div, Html, node, text)
 import Html.Attributes exposing (..)
-import Material.Table exposing (..)
 
 
+--import Material.Table exposing (..)
 -- TYPES
 
 
 type alias Row =
     { name : String
     , assignment : String
-    , followUpDate : String
+    , followUpDate : { value : Maybe Date, state : DateTimePicker.State }
     }
+
+
+
+-- MSG
+
+
+type Msg
+    = DateChanged DateTimePicker.State (Maybe Date)
 
 
 
 -- MODEL
 
 
-model : List Row
-model =
-    [ { name = "Reckia, Jackson", assignment = "Write your plans to have meaningful prayer and scripture study", followUpDate = "05/01/2017" }
-    , { name = "Reckia, Jackson", assignment = "Write your plans to have meaningful prayer and scripture study", followUpDate = "05/01/2017" }
-    , { name = "Reckia, Jackson", assignment = "Write your plans to have meaningful prayer and scripture study", followUpDate = "05/01/2017" }
-    ]
+initialFollowUpDate : { state : State, value : Maybe Date }
+initialFollowUpDate =
+    { value = Nothing, state = DateTimePicker.initialState }
+
+
+init : ( Row, Cmd Msg )
+init =
+    ( { name = "Reckia, Jackson"
+      , assignment = "Write your plans to have meaningful prayer and scripture study"
+      , followUpDate = initialFollowUpDate
+      }
+    , DateTimePicker.initialCmd DateChanged DateTimePicker.initialState
+    )
+
+
+update : Msg -> Row -> ( Row, Cmd Msg )
+update msg row =
+    case msg of
+        DateChanged state date ->
+            let
+                followUpDate =
+                    row.followUpDate
+            in
+                ( { row
+                    | followUpDate =
+                        { followUpDate
+                            | value = date
+                            , state =
+                                state
+                        }
+                  }
+                , Cmd.none
+                )
 
 
 
 -- VIEW
 
 
-view : Html msg
-view =
+view : Row -> Html Msg
+view row =
     div [ style [ ( "margin", "50px auto" ), ( "max-width", "50%" ) ] ]
         [ node "link"
             [ rel "stylesheet"
@@ -47,26 +83,34 @@ view =
                 "https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
             ]
             []
-        , table []
-            [ thead []
-                [ tr []
-                    [ th [] [ text "Name" ]
-                    , th [] [ text "Assignment" ]
-                    , th [] [ text "Follow-up Date" ]
-                    ]
-                ]
-            , tbody []
-                (model
-                    |> List.map
-                        (\item ->
-                            tr []
-                                [ td [] [ text item.name ]
-                                , td [] [ text item.assignment ]
-                                , td [] [ text item.followUpDate ]
-                                ]
-                        )
-                )
-            ]
+        , DateTimePicker.datePicker
+            DateChanged
+            []
+            row.followUpDate.state
+            row.followUpDate.value
+
+        {-
+           , table []
+               [ thead []
+                   [ tr []
+                       [ th [] [ text "Name" ]
+                       , th [] [ text "Assignment" ]
+                       , th [] [ text "Follow-up Date" ]
+                       ]
+                   ]
+               , tbody []
+                   (model
+                       |> List.map
+                           (\item ->
+                               tr []
+                                   [ td [] [ text item.name ]
+                                   , td [] [ text item.assignment ]
+                                   , td [] [ text item.followUpDate ]
+                                   ]
+                           )
+                   )
+               ]
+        -}
         ]
 
 
@@ -74,6 +118,11 @@ view =
 -- UPDATE
 
 
-main : Html msg
+main : Program Never Row Msg
 main =
-    view
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
